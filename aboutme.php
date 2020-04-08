3dc13955283aeeac5e7a68052067ce420d9a8ef5
yashintafazrin <55775810+yashintafazrin@users.noreply.github.com>
2020-04-08 11:59:47 +0700

    Menambahkan Nomor HP,email & Menghapus Alamat

--- aboutme.php
+++ aboutme.php
@@ -2,5 +2,6 @@
 echo "Nama : Yashinta Fazrin";
 echo "Kelas : XI RPL 2";
 echo "Asal Sekolah : SMK Taruna Bhakti Depok";
-echo "Alamat :Kp.Babakan Rawa Kalong RT 02/08 No:36 Kel.Curug,Kec.Cimanggis,Kota.Depok";
+echo "Nomor HP : 089602961203";
+echo "Email : yastafazrin@gmail.com";
 ?>
