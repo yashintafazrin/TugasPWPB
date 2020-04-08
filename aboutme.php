131da3857c4cef686ba4265febd4f32cdb78de9d
yashintafazrin <55775810+yashintafazrin@users.noreply.github.com>
2020-04-08 11:47:23 +0700

    Membuat file aboutme.php

---
+++ aboutme.php
@@ -0,0 +1,6 @@
+<?php 
+echo "Nama : Yashinta Fazrin";
+echo "Kelas : XI RPL 2";
+echo "Asal Sekolah : SMK Taruna Bhakti Depok";
+echo "Alamat :Kp.Babakan Rawa Kalong RT 02/08 No:36 Kel.Curug,Kec.Cimanggis,Kota.Depok";
+?>
